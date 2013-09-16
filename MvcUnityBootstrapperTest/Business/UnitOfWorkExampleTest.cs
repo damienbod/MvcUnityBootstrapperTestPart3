c4f08d83d94a0493eb1d926d297e10f1f97f9eea
damienbod <damien_bod@hotmail.com>
2013-09-16 13:10:51 +0200

    init repo

---
+++ MvcUnityBootstrapperTest/Business/UnitOfWorkExampleTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcUnityBootstrapperTest.Logging;
+
+namespace MvcUnityBootstrapperTest.Business
+{
+    public class UnitOfWorkExampleTest : IUnitOfWorkExample
+    {
+        static int _counter = 0; 
+        public UnitOfWorkExampleTest()
+        {
+            _counter++;
+            UnityEventLogger.Log.CreateUnityMessage("UnitOfWorkExampleTest " + _counter );
+        }
+
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            UnityEventLogger.Log.LogUnityMessage("Entered Dispose" + _counter);
+            if (!_disposed)
+            {
+                _counter--;
+                UnityEventLogger.Log.DisposeUnityMessage("UnitOfWorkExampleTest " + _counter);
+                _disposed = true;
+            }
+        }
+
+        public string HelloFromUnitOfWorkExample()
+        {
+            UnityEventLogger.Log.LogUnityMessage("Hello UnitOfWorkExampleTest " + _counter);
+            return "HelloFromUnitOfWorkExample";
+        }
+    }
+}
---
+++ MvcUnityBootstrapperTest/UnityExtensions/UnityHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Utility;
+
+namespace MvcUnityBootstrapperTest.UnityExtensions
+{
+    public static class UnityHelpers
+    {
+        private static readonly Type[] EmptyTypes = new Type[0];
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "implementationType", Justification = "Need to match signature Func<Type, IEnumerable<Type>>")]       
+        public static IEnumerable<Type> FromAllInterfacesWith_PerRequestLifetimeAttribute(Type implementationType)
+        {
+            Guard.ArgumentNotNull(implementationType, "implementationType");
+
+            if (implementationType.GetCustomAttributes(typeof(UnityIoCPerRequestLifetimeAttribute), true).Length > 0)
+            {
+                var implementationTypeAssembly = implementationType.GetTypeInfo().Assembly;
+                var typeInfo = implementationType.GetTypeInfo();
+                return typeInfo.ImplementedInterfaces;
+            }
+            else
+            {
+                return EmptyTypes;
+            }
+        }
+
+        /// <summary>
+        /// Returns no types.
+        /// </summary>
+        /// <param name="implementationType">The type to register.</param>
+        /// <returns>An empty enumeration.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "implementationType", Justification = "Need to match signature Func<Type, IEnumerable<Type>>")]
+        public static IEnumerable<Type> None(Type implementationType)
+        {
+            return EmptyTypes;
+        }
+    }
+}
